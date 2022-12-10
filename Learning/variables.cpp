542ff2986a740b7859af349edd558d55cbd1956d
Anshul Jain <samgalacestyle01@gmail.com>
2022-12-11 00:46:59 +0530

    Routine Commit

---
+++ Learning/variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int file_size = 100;
+    // int file_size;
+    double sales = 9.99;
+    // cout << "FileSize: " + file_size;
+    cout << file_size;
+    cout << "\n\n";
+    cout << sales;
+    cout << "\n\n";
+    // cout << "Sales: " + sales;
+    return 0;
+}
